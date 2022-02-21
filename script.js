7905dd80e3689ae42f6421c42ffc92c06297a27a
laskor <al.moksud@gmail.com>
2022-02-20 23:59:44 -0500

    alert added

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-// input function
+// input 
 function getInput(inputId){
     const input = document.getElementById(inputId);
     const inputText = input.value;
@@ -13,7 +13,7 @@ function previousAmountTotal(startingAmount, amount){
     const preAmount = document.getElementById(startingAmount);
     const preAmountText = preAmount.innerText;
     const totalPreAmount = parseFloat(preAmountText);
-    // adding deposit to balance
+    // updateing previous amount to new one
     preAmount.innerText = totalPreAmount + amount;
 }
 // balance
@@ -21,27 +21,35 @@ function balanceAmountTotal(preBalance, amount, isAdd){
     const preBalanceAmount = document.getElementById(preBalance);
     const preBalanceAmountText = preBalanceAmount.innerText;
     const totalPreBalanceAmount = parseFloat(preBalanceAmountText);
-    
+
     if(isAdd == true){
         preBalanceAmount.innerText = totalPreBalanceAmount + amount;
     }
     else{
         preBalanceAmount.innerText = totalPreBalanceAmount - amount;
     }
+
+    // alert
+    if(getInput('withdraw-input') > preBalanceAmount){
+        const fail = document.getElementById('notify-fail');
+        fail.style.display = 'block';
+    }
 }
 
-// calculation
+// deposit button
 document.getElementById('deposit-btn').addEventListener('click', function (){
     // getting deposit input
     const depositInput = getInput('deposit-input');
-    
-    // adding previous deposit  to new one
-    previousAmountTotal('deposit-amount', depositInput);
 
-    // adding  deposit to total balance
-    balanceAmountTotal('balance-amount', depositInput, true);
+    if(depositInput > 0){
+        // adding previous deposit  to new one
+        previousAmountTotal('deposit-amount', depositInput);
+
+        // adding  deposit to total balance
+        balanceAmountTotal('balance-amount', depositInput, true);
+    }
 })
-// withdraw
+// withdraw button
 document.getElementById('withdraw-btn').addEventListener('click', function (){
 
     // getting withdraw input
@@ -52,4 +60,23 @@ document.getElementById('withdraw-btn').addEventListener('click', function (){
 
      // sub withdrawl from  total balance
     balanceAmountTotal('balance-amount', withdrawInput);
+     
 })
+// // alert
+document.getElementById('deposit-input').addEventListener('keyup', function(event){
+    const value = event.target.value;
+    if(isNaN(value)){
+        const failed = document.getElementById('fail');
+        failed.style.display = 'block';
+    }
+})
+document.getElementById('withdraw-input').addEventListener('keyup', function(event){
+    const value = event.target.value;
+    if(isNaN(value)){
+        const failed = document.getElementById('fail');
+        failed.style.display = 'block';
+    }
+})
+
+
+
